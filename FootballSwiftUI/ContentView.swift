a6cbbd60adad200b776a774fc876ac7fdf7481a7
OlegPlugaru <olegplugaru.md@gmail.com>
2024-01-06 13:39:25 +0200

    Initial Commit

---
+++ FootballSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FootballSwiftUI
+//
+//  Created by Oleg Plugaru on 06.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FootballSwiftUI/FootballSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballSwiftUIApp.swift
+//  FootballSwiftUI
+//
+//  Created by Oleg Plugaru on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FootballSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
